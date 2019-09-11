9df35019a8e15817c66addc00ce07130685b3e9a
favorable-device-5158 <blee2125@gmail.com>
2019-09-11 12:38:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,10 +57,12 @@ class TicTacToe
     won? || full?
   end
   def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board[combo[0]] == @board[combo[1]] &&
-      @board[combo[1]] == @board[combo[2]] &&
-      position_taken?(combo[0])
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) &&
+        @board[combo[0]] == @board[combo[1]] &&
+        @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
     end
   end
   def draw?
